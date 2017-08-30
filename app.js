9447af32aaf027595f2aea7195ab9d569219abbb
mbistuer <m.bistuer.sallan@gmail.com>
2017-08-30 18:24:46 +0200

    heroku-icon

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const favicon = require('serve-favicon')
 const path = require('path');
 let app = express();
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 //midedlewares
 
