43305a0286ff395e933e68708256d24572ce9c1d
Mateo Bistuer <mbistuer@Itteria.com>
2018-04-27 13:56:53 +0200

    version 2.0

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-'use strict'
+'use strict';
 
 const express = require('express');
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const favicon = require('serve-favicon')
+const favicon = require('serve-favicon');
 const path = require('path');
 let app = express();
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 //midedlewares
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -18,11 +18,13 @@ app.use(cors());
 const userRoutes = require('./routes/user');
 const videoRoutes = require('./routes/videos');
 const followRoutes = require('./routes/follow');
-app.use('/',express.static('client',{redirect:false}));
-app.use('/api',userRoutes);
-app.use('/api',videoRoutes);
-app.use('/api',followRoutes);
-app.get('*',(req,res) => {
+const tagRoutes = require('./routes/tags');
+app.use('/', express.static('client', { redirect: false }));
+app.use('/api', userRoutes);
+app.use('/api', videoRoutes);
+app.use('/api', followRoutes);
+app.use('/api', tagRoutes);
+app.get('*', (req, res) => {
   res.sendFile(path.resolve('client/index.html'));
 });
- module.exports = app;
+module.exports = app;
